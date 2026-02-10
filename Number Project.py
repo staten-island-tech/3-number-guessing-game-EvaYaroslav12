c9b30f5fd9c161a55010c3fc74679df716764a48
EvaYaroslav12 <eva.yaroslavskiy@gmail.com>
2026-02-10 13:15:41 -0500

    fail

--- Number Project.py
+++ Number Project.py
@@ -6,7 +6,7 @@ inp = input ("Guess a number between 1 and 10: ")
 imp = int(inp)
 
 while random_int != imp:
-    print ('incorrect')
+    print (imp)
     if imp > random_int:
         print ('A little lower!')
     if imp < random_int:
@@ -14,3 +14,4 @@ while random_int != imp:
     inp = input ("Guess a number between 1 and 10: ")
 
 print ('correct!')
+    
