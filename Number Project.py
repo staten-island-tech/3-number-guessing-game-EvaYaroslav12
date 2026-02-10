3a909257d3fa8db72471fdc2a8186ce2fc993342
EvaYaroslav12 <eva.yaroslavskiy@gmail.com>
2026-02-10 14:49:37 -0500

    Final

--- Number Project.py
+++ Number Project.py
@@ -3,9 +3,11 @@ import random
 random_int = random.randint(1,10)
 inp = input ("Guess a number between 1 and 10: ")
 imp = int(inp)
+user_list = []
 
 while random_int != imp:
-    print ('incorrect')
+    user_list.append(imp)
+    print('you have already guessed: ', user_list)
     if imp > random_int:
         print ('A little lower!')
     if imp < random_int:
@@ -14,3 +16,4 @@ while random_int != imp:
     imp = int(inp)
 
 print ('correct!')
+print ('your guesses: ', user_list)
